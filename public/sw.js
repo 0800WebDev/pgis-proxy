e024c7a4959f87fe391bae99ba522415db12ca3e
0800 <spmspy0800@gmail.com>
2026-05-01 06:47:26 +0000

    Edit sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,30 @@
+const SECRET_KEY = "k7Xm2#pQ9nLw4@Rz"; // must match index.js exactly
+
+function xorCipher(str, key) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        result += String.fromCharCode(
+            str.charCodeAt(i) ^ key.charCodeAt(i % key.length)
+        );
+    }
+    return result;
+}
+
+function encode(url) {
+    if (!url) return url;
+    return btoa(xorCipher(url, SECRET_KEY))
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=/g, "");
+}
+
+function decode(encoded) {
+    if (!encoded) return encoded;
+    let b64 = encoded.replace(/-/g, "+").replace(/_/g, "/");
+    while (b64.length % 4) b64 += "=";
+    return xorCipher(atob(b64), SECRET_KEY);
+}
+
 importScripts("/scram/scramjet.all.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
